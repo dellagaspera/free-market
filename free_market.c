79ed3182cde150991f640b3ac8a4c18fa7caffe0
Gabriel Della Gaspera <gabriel.gaspera@gmail.com>
2024-09-22 22:05:36 -0300

    Update free_market.c
    - constantes e inclusões feitas e comentadas
    - estruturas feitas e comentadas

--- free_market.c
+++ free_market.c
@@ -1,35 +1,49 @@
-//  INCLUSOES
+    //  INCLUSOES   //
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-//  CONSTANTES ARQUIVOS
-#define ARQUIVO_USUARIOS "fm_usuarios.bin"
-#define ARQUIVO_PRODUTOS "fm_produtos.bin"
-#define ARQUIVO_AVALIACOES "fm_avaliacoes.bin"
-#define ARQUIVO_FAVORITOS "fm_favoritos.bin"
+    //  CONSTANTES ARQUIVOS   //
 
-//  CONSTANTES USUARIOS
-#define CARACTERES_EMAIL (30 + 1)
-#define CARACTERES_NOME_EXIBICAO (30 + 1)
-#define CARACTERES_ID (20 + 1)
-#define CARACTERES_SENHA (16 + 1)
+#define ARQUIVO_USUARIOS "fm_usuarios.bin" // Arquivo onde estão armazendados os usuários
+#define ARQUIVO_PRODUTOS "fm_produtos.bin" // Arquivo onde estão armazendados os produtos
+#define ARQUIVO_AVALIACOES "fm_avaliacoes.bin" // Arquivo onde estão armazendados as avaliações
 
-//  CONSTANTES PRODUTOS
-#define CARACTERES_NOME_PRODUTO (30 + 1)
-#define CARACTERES_DESCRICAO_PRODUTO
+    //  CONSTANTES USUARIOS   //
 
-//  CONSTANTES AVALIACOES
-#define CARACTERES_MENSAGEM (300 + 1)
+#define CARACTERES_EMAIL (30 + 1) // Máximo de caracteres de um e-mail
+#define CARACTERES_NOME_EXIBICAO (30 + 1) // Máximo de caracteres de um nome de exibição
+#define CARACTERES_ID (20 + 1)  // Máximo de caracteres de um identificador
+#define CARACTERES_SENHA (16 + 1)  // Máximo de caracteres de um asenha
 
-//  CONSTANTES RETORNO
+    //  CONSTANTES PRODUTOS   //
+
+#define CARACTERES_NOME_PRODUTO (30 + 1)  // Máximo de caracteres de um nome de produto
+#define CARACTERES_DESCRICAO_PRODUTO (255 + 1)  // Máximo de caracteres de uma descrição de produto
+
+    //  CONSTANTES AVALIACOES   //
+
+#define CARACTERES_MENSAGEM (300 + 1)  // Máximo de caracteres de uma avaliação
+
+    //  CONSTANTES RETORNO   //
+
 #define SUCESSO 0
 
-//  CONSTANTES GERAIS
+    //  CONSTANTES GERAIS   //
+
 #define true  (1!=0)
 #define false (1==0)
 
-//  ESTRUTURAS
+    //  ESTRUTURAS   //
+    
+/* 
+    #### Usuário
+    - email : E-mail do usuário
+    - senha : Senha do usuário
+    - nomeExibicao: Nome de exibição do usuário
+    - id: Identificador do usuário
+*/
 typedef struct usuarios_e {
     char email[CARACTERES_EMAIL];
     char senha[CARACTERES_SENHA];
@@ -37,26 +51,43 @@ typedef struct usuarios_e {
     char id[CARACTERES_ID];
 } usuarios_t;
 
+/* 
+    #### Produto
+    - idVendedor : Identificador do vendedor
+    - id : Identificador do produto
+    - nome: Nome do produto
+    - nota: Nota média do produto
+*/
 typedef struct produtos_e {
     char idVendedor[CARACTERES_ID];
     char id[CARACTERES_ID];
     char nome[CARACTERES_NOME_PRODUTO];
     char descricao[CARACTERES_DESCRICAO_PRODUTO];
     int  nota;
-}
+} produtos_t;
 
-typedef struct avaliacoes_e {
+/*
+    #### Avaliacao
+    - idAvaliacao: Identificador da avaliação
+    - idProduto: Identificador do produto avaliado
+    - idComprador: Identificador do comprador que avaliou
+    - mensagem: Mensagem da avaliação
+    - nota: Nota dada para o produto
+*/
+typedef struct avaliacao_e {
     char idAvaliacao[CARACTERES_ID];
     char idProduto[CARACTERES_ID];
     char idComprador[CARACTERES_ID];
-    int  nota;
     char mensagem[CARACTERES_MENSAGEM];
-} avaliacoes_t;
+    int  nota;
+} avaliacao_t;
 
-//  FUNCOES
+//  FUNCOES   //
+
 
 
 //  MAIN
+
 int main(int argc, char ** argv) {
 
     return SUCESSO;
